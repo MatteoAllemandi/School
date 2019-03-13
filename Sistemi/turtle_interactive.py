940e3b804889c92b70c39b2e14b690f29f5a461c
MatteoAllemandi <46619805+MatteoAllemandi@users.noreply.github.com>
2019-03-13 22:35:25 +0100

    Matteo Allemandi
    Versione aggiornata con chiusura finestra dopo un certo quantitativo di tempo

--- Sistemi/turtle_interactive.py
+++ Sistemi/turtle_interactive.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import time 
 
 passo=15
 gradi=90
@@ -7,18 +8,22 @@ turtle.color("red","blue")
 turtle.speed(1)
 
 while(True) :
-    print("comando(f/b/l/r): ")
+    print("inserisci un comando (f/b/l/r) oppure q per uscire: ")
     com=input()
 
-    if (com=="f"):
+    if (com == "f"):
         turtle.forward(passo)
-    elif (com=="b"):
+    elif (com == "b"):
         turtle.back(passo)
-    elif (com=="l"):
+    elif (com == "l"):
         turtle.left(gradi)
-    elif (com=="r"):
+    elif (com == "r"):
         turtle.right(gradi)
+    elif (com == "q"):
+        break
     else:
         print("comando inesistente")
-    
-turtle.done()
+
+#turtle.done()      #toggle comment for the permanent turtle screen after exit with 'q' 
+time.sleep(2)
+turtle.Screen.bye() #close the turtle screen after exit
