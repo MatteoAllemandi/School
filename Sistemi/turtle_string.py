be423777d3757a9d5042979ad8feee73bf425e2f
MatteoAllemandi <46619805+MatteoAllemandi@users.noreply.github.com>
2019-03-14 09:25:17 +0100

    Matteo Allemandi
    Migliorie

--- Sistemi/turtle_string.py
+++ Sistemi/turtle_string.py
@@ -1,19 +1,19 @@
 import turtle
 import random
 
-print("inserisci le indicazioni per la tartaruga: ")        #input for turtle's path
+print("inserisci le indicazioni per la tartaruga(f/b/l/r): ")        #input for turtle's path
 dir=input()
 
 passo=10                    #set speed,distance for each forward and color
 turtle.speed(1)
 turtle.color("red","blue")
 
-for k in range(len(dir)):
-    if(dir[k] == "f"):
+for k in dir:
+    if(k == "f"):
         turtle.forward(passo)
-    elif dir[k] == "r":
+    elif k == "r":
         turtle.right(90)
-    elif dir[k] == "l":
+    elif k == "l":
         turtle.left(90)
     else:
         turtle.back(passo)
