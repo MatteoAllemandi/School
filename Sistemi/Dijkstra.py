5e79beb0e47dae14ea20e0ac7136f6226b948640
MatteoAllemandi <46619805+MatteoAllemandi@users.noreply.github.com>
2019-04-15 17:47:45 +0200

    Matteo Allemandi
    Dijkstra finito

--- Sistemi/Dijkstra.py
+++ Sistemi/Dijkstra.py
@@ -1,6 +1,7 @@
+
 import math
 
-grafo = [[0,1,4,0,0,0,0,0],
+graph = [[0,1,4,0,0,0,0,0],
          [1,0,0,0,0,2,0,0],
          [4,0,0,5,0,0,0,0],
          [0,0,5,0,1,3,0,0],
@@ -9,26 +10,35 @@ grafo = [[0,1,4,0,0,0,0,0],
          [0,0,0,0,2,0,0,4],
          [0,0,0,0,3,0,4,0]]
 
-labelList = [math.inf] * len(grafo) #inizializzazione
-labelList[0] = 0    #distanza da nodo sorgente uguale a 0
+labelList = [math.inf] * len(graph) #inizializzazione delle label (moltiplico per il numero di nodi per le righe)
+labelList[0] = 0                    #distanza dal nodo sorgente = 0
 
-nodiDaEsplorare = [0,1,2,3,4,5,6,7]
+unexploredNode = [0,1,2,3,4,5,6,7]   #nodi da esplorare
 
-while len(nodiDaEsplorare)>0:
-    cont = 0
-    near_list = []
-    k = min(labelList)
+cntPasso=0
 
-    for i in labelList[k]:
-        if(i!=0):
-            near_list.append(cont)
-        cont+=1
-
-    for i in labelList:
-        
+while len(unexploredNode)>0:
+    print("Passo numero: %d"% cntPasso)
+    print("Nodi inesplorati: ", unexploredNode)
+    print("Lista delle label: ", labelList)
+    print("-----------------------------------------------")
+    #scelgo il nodo con label minore(k)
+    infiniteList = [math.inf] * len(graph)
     
-    # Scelgo il nodo che ha la label più piccola (k).
-    # Ricerco i nodi collegati al nodo k -> ciclo for sulla riga k di grafo
-    # per ogni nodo sostituisco il valore(label di k + peso dell'arco verso nodo vicino)
-    # della label SOLO se minore di quello presente.
-    # 
+    for i in unexploredNode:
+        infiniteList[i]=labelList[i]    #valore della labelList[i] 
+    
+    minimun = min(infiniteList)   #trovo il minimo
+
+    if(minimun==math.inf):  #controllo per uscire dal ciclo
+        break
+    
+    currentNode = infiniteList.index(minimun)  #prendo l'indice del valore minimo
+    for node, weight in enumerate(graph[currentNode]):       #ciclo for con ricerca dei nodi collegati a k per indici non ancora esplorati
+        if(weight!=0):   #verifico che sia collegato e che non sia se stesso
+            newLabel = labelList[currentNode] + weight
+            if(newLabel<labelList[node]):
+               labelList[node] = newLabel     #per ogni nuovo nodo sommo (valore label(k)+peso arco di quel nodo) il valore della sua label e controllo se sono minori così sostituisco sennò niente
+    
+    cntPasso=cntPasso+1
+unexploredNode.remove(currentNode) #rimuovo l'indice visitato   
