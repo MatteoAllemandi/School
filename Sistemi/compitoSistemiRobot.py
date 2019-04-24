6b06f304aaefbe3e2b27014e9fbd8a4339a15dbd
MatteoAllemandi <46619805+MatteoAllemandi@users.noreply.github.com>
2019-04-24 17:34:14 +0200

    Matteo Allemandi

--- Sistemi/compitoSistemiRobot.py
+++ Sistemi/compitoSistemiRobot.py
@@ -1,8 +1,8 @@
 import math
 
-'''
-Funzione per calcolare dizionario delle adiacenze
-def adj_dict(graph):
+
+#Funzione per calcolare dizionario delle adiacenze
+def adj_dictionary(graph):
     adj_dictionary = {}
     nodo = -1
 
@@ -19,7 +19,7 @@ def adj_dict(graph):
         adj_dictionary[str(nodo)] = confinanti
 
     return adj_dictionary  
-'''
+
 
 adj_dict={  0:[1,2],
             1:[0,5],
@@ -30,10 +30,12 @@ adj_dict={  0:[1,2],
             6:[4,7],
             7:[4,6]}  
 
-def dijkstra(graph,initial,finish,dim):
+def dijkstra(graph,initial,finish,dim,toggle):
     labelList = [math.inf] * len(graph) #inizializzazione delle label (moltiplico per il numero di nodi per le righe)
     labelList[initial] = 0                    #distanza dal nodo sorgente = 0
     unexploredNode = []   #nodi da esplorare
+    orderVisit = []
+    tro = False
 
     for cont in range(dim):
         unexploredNode.append(cont)
@@ -65,16 +67,24 @@ def dijkstra(graph,initial,finish,dim):
         
         cntPasso=cntPasso+1
         unexploredNode.remove(currentNode) #rimuovo l'indice visitato
+        if(currentNode == finish):
+            tro = True
+            orderVisit.append(currentNode)
 
-    print("la distanza dal nodo %s di partenza ed il nodo %s di arrivo e' --> %s" % (initial,finish,labelList[finish]))
+        if(tro != True):
+            orderVisit.append(currentNode)
+
+
+    if (toggle == True):
+        print("\n la distanza dal nodo %s di partenza ed il nodo %s di arrivo e' --> %s \n" % (initial,finish,labelList[finish]))
     
-    return labelList
+    return labelList,orderVisit
 
 def generateGraph(adj_dict):
     nodeNumber=0
     graph=[]
-    for key,value in adj_dict.items():      #stampo il dizionario
-        print("%s --> %s" %(key,value))
+    for key,value in adj_dict.items():      #stampo il dizionario e conto i nodi
+        #print("%s --> %s" %(key,value))
         nodeNumber+=1
     
     #graph = [[False]*nodeNumber]*nodeNumber
@@ -83,15 +93,24 @@ def generateGraph(adj_dict):
     
     for key,value in adj_dict.items():
         for k in value:
-            graph[key][k] = True        #scorrendo la ista assegno true alle celle dei nodi collegati fra loro così da creare l grafo
+            graph[key][k] = True        #scorrendo la ista assegno true alle celle dei nodi collegati fra loro così da creare il grafo
 
     return graph,nodeNumber
+'''
+def calcolaPercorsi(graph,dim,start):       #calcolo il percorso fra il nodo di partenza e tutti gli altri nodi
+    distances = []
 
-
+    for j in range(dim):
+        if(start != j):
+            _,distances = dijkstra(graph,start,j,dim,False)
+            print("percorso più breve di %s e %s --->  %s" %(start,j,distances))
+'''
 start = int(input("inserire il nodo di partenza: "))
 finish = int(input("inserire il nodo di arrivo: "))
 
 graph,dim = generateGraph(adj_dict)
 
-dijkstra(graph,start,finish,dim)
-#print(graph)
+dijkstra(graph,start,finish,dim,True)
+
+
+calcolaPercorsi(graph,dim,start)
