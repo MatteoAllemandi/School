731fc08cd66358b1bb43e03b81e5e658e89e994a
MatteoAllemandi <46619805+MatteoAllemandi@users.noreply.github.com>
2019-05-29 18:52:31 +0200

    Matteo Allemandi

--- Sistemi/socket/clientUDP.py
+++ Sistemi/socket/clientUDP.py
@@ -1,19 +1,18 @@
 import socket as sck 
 
-HOST,PORT = "127.0.0.1","1984"
-
+HOST = "127.0.0.1"
+PORT = 5000
 s = sck.socket(sck.AF_INET,sck.SOCK_DGRAM)
-s.bind((HOST,5000))
 
 while True:
     testo = input("inserisci il messaggio: ")
-    s.sendto(testo.encode(),HOST)
+    s.sendto(testo.encode(),(HOST,PORT))
+
+    if(str(testo) == "stop"):
+        break
 
     data,server = s.recvfrom(4096)
     print("ricevuto --> " + str(data) + "from --> " + str(server))
-
-    if(str(data) == "stop"):
-        break
 
 s.close()
 print("socket chiuso")
