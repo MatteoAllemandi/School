fb50c4fb6af0a42cc0ee635c3fa6b7a95f47ba3d
MatteoAllemandi <46619805+MatteoAllemandi@users.noreply.github.com>
2019-05-29 18:53:02 +0200

    Matteo Allemandi

--- Sistemi/socket/serverUDP.py
+++ Sistemi/socket/serverUDP.py
@@ -1,9 +1,10 @@
 import socket as sck 
 
-HOST,PORT = "127.0.0.1","1984"
+HOST = "127.0.0.1"
+PORT = 5000
 
 s = sck.socket(sck.AF_INET,sck.SOCK_DGRAM)
-s.bind((HOST,5000))
+s.bind((HOST,PORT))
 
 while True:
     data,address = s.recvfrom(4096)
@@ -12,8 +13,8 @@ while True:
     if(str(data) == "stop"):
         break
 
-    text = input("inserisci risposta: ")
-    s.sendto(text.encode(),HOST)
+    #text = input("inserisci risposta: ")
+    #s.sendto(text.encode(),address)
     
 s.close()
 print("chiusura server")
